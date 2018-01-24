8c991c94af02c6a71d1d3423f56860f0d12c4311
Luke Vance <luke@cloud-elements.com>
2018-01-23 22:22:26 -0700

    added error catch to instanceBody

--- src/ce-util.js
+++ src/ce-util.js
@@ -1,25 +1,30 @@
 // Function to return the correct instance body for a given vendor, this is an area where individual nuances pertaining a vendor (Element) must be known - documentation for these nuances can be found here: https://developers.cloud-elements.com/docs/elements.html
 // At the end of the authentication flow, the correct vendor code/token/whatever needs to be sent to Cloud Elements in order to successfully return an `Element Token` to be used to make API calls against the correct vendor instance
 const createInstanceBody = (elementKey, code, appURL, vendorData) => {
-    let postInstanceBody = {
-                "element": {
-                    "key": elementKey
-                },
-                "providerData": {
-                    "code": code
-                },
-                "configuration": {
-                    "authentication.type": "oauth2",
-                    "oauth.callback.url": (appURL || process.env.APP_URL),
-                    "oauth.api.key": vendorData.vendorApiKey,
-                    "oauth.api.secret": vendorData.vendorSecret
-                },
-                "tags": [
-                    "saas_demo_boilerplate"
-                ],
-                "name": "SAAS_DEMO_" + (new Date()).getTime()
-            };
-        return postInstanceBody;
+    if (elementKey && code && vendorData) {
+        let postInstanceBody = {
+            "element": {
+                "key": elementKey
+            },
+            "providerData": {
+                "code": code
+            },
+            "configuration": {
+                "authentication.type": "oauth2",
+                "oauth.callback.url": (appURL || process.env.APP_URL),
+                "oauth.api.key": vendorData.vendorApiKey,
+                "oauth.api.secret": vendorData.vendorSecret
+            },
+            "tags": [
+                "saas_demo_boilerplate"
+            ],
+            "name": "SAAS_DEMO_" + (new Date()).getTime()
+        };
+    return postInstanceBody;
+    } else {
+        throw "missing required parameters for instance body";
+    }
+   
 };
 
 
