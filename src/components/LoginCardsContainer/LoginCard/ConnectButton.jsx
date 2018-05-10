e374868367c2870d32c042ca68031fdd89a0fdc0
Luke Vance <luke@cloud-elements.com>
2018-05-10 10:55:16 -0600

    got rid of button laoding and fixed state update on redirect

--- src/components/LoginCardsContainer/LoginCard/ConnectButton.jsx
+++ src/components/LoginCardsContainer/LoginCard/ConnectButton.jsx
@@ -4,21 +4,15 @@ import Button from 'material-ui/Button';
 class ConnectButton extends Component {
 
     render() {
-        const { connected, redirectUrl } = this.props;
+        const { connected, oauthRedirectSend } = this.props;
         // const { redirectUrl } = this.state;
 
-        if (!connected && !redirectUrl) {
-            return (
-                <Button raised>
-                    Loading...
-                </Button>
-            )
-        } else if (!connected) {
+        if (!connected) {
             return (
                 <Button
                     raised
-                    onClick={function () {
-                        window.location = redirectUrl
+                    onClick={function(){
+                        oauthRedirectSend();
                     }}
                 >
                     Connect
